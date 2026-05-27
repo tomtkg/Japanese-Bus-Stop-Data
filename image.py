ec6fce20f6e7a1832c77afcf7b4d620f74e08242
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2026-05-27 22:17:57 +0900

    Update image.py

--- image.py
+++ image.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize, to_hex
 from japanmap import picture
-from prefectures import PREF_POS
+from prefecture import PREF_POS
 
-def save_image(name: str, dic: dict[int, int]) -> None:
+def save_image(name: str, vec: list[int]) -> None:
+    file_name = "images/"+name+".png"
     bbox = dict(facecolor='white', alpha=.5, edgecolor='none', pad=0)
     cmap = plt.get_cmap("Reds")
-    norm = Normalize(min(dic.values()), max(dic.values()))
-    
+    norm = Normalize(min(vec), max(vec))
+    img = picture({k: to_hex(cmap(norm(v))) for k, v in enumerate(vec, 1)})
+
     plt.figure(figsize=(10, 8))
     plt.axis('off')
-    plt.imshow(picture({k: to_hex(cmap(norm(v))) for k, v in dic.items()}))
-    for k, v in dic.items():
+    plt.imshow(img)  # type: ignore
+    for k, v in enumerate(vec, 1):
         plt.text(*PREF_POS[k], str(v), ha='center', va='center', bbox=bbox)
-    plt.savefig("images/"+name, dpi=600, bbox_inches='tight', pad_inches=0)
+    plt.savefig(file_name, dpi=600, bbox_inches='tight', pad_inches=0)
+    
+    
